319cc8696c3f94c28d5d73ca4dd08ee0a5f7e47e
konrdekat <kondratenko.es@gmail.com>
2016-11-09 21:52:44 +0700

    Update ContEl.js

--- app/components/ContEl.js
+++ app/components/ContEl.js
@@ -1,25 +1,35 @@
 var React = require('react');
 var TextField = require ('material-ui/TextField').default;
 
+var unicKey = "TOpwJkKLwV";
+
 var ContEl = React.createClass({
 	getInitialState: function(){
 		return {
 			userInput: this.props.text
+			//oldValue: this.props.text
 		};
 	},
-
+	componentWillReceiveProps: function (nextProps) {
+		this.setState({
+		  userInput: nextProps.text
+		});
+	},
 	handleUserInput: function (e) {
-		var returnObj = JSON.parse(localStorage.getItem(this.props.stKey));
-		obj[this.props.objType] = e.target.value;
-		var serialObj = JSON.stringify(obj);
-		try {
-		  localStorage.setItem(this.props.stKey, serialObj); 
-		} catch (e) {
-		  if (e === QUOTA_EXCEEDED_ERR) {
-		   alert('Превышен лимит');
-		  }
+		var contacts = localStorage.getItem(unicKey).split("%"); //Get all contacts as array
+		for(var i = 0; i < contacts.length; i++){
+			if(contacts[i].indexOf(this.props.itemId) != -1){
+				var tmp = JSON.parse(contacts[i]);
+				tmp[this.props.objType] = e.target.value; 
+				contacts[i] = JSON.stringify(tmp);
+			}
 		}
-		
+		localStorage.removeItem(unicKey);
+		var newValue = "";
+		for (var i = 0; i < contacts.length; i++){
+			newValue += contacts[i] + "%";
+		}
+		localStorage.setItem(unicKey, newValue);
 		this.setState({
 		  userInput: e.target.value
 		});
