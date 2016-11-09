b9e8b31c337774ed96b5bff0f0067b4df9550efa
konrdekat <kondratenko.es@gmail.com>
2016-11-09 21:54:16 +0700

    Update Search.js

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var TextField = require ('material-ui/TextField').default;
 
+var unicKey = "TOpwJkKLwV";
+
 var Search = React.createClass({
 	getInitialState: function(){
 		return {
@@ -11,18 +13,21 @@ var Search = React.createClass({
 	  handleUserInput: function (e) {
 			var resOfSearch = '';
 			if(e.target.value != ''){
-				for(var i = 0; i < localStorage.length; i++){
-					var itKey = localStorage.key(i);
-					var returnObj = JSON.parse(localStorage.getItem(itKey));
-					var substr = e.target.value;
-					if ((returnObj.name.indexOf(substr) != -1) || (returnObj.phone.indexOf(substr) != -1) || (returnObj.email.indexOf(substr) != -1) || (returnObj.address.indexOf(substr) != -1)){
-						resOfSearch += itKey + ',';
+				var contacts = localStorage.getItem(unicKey).split("%"); //Get all contacts as array
+				for(var i = 0; i < contacts.length; i++){
+					if(contacts[i] != ""){ 
+						var tmp = JSON.parse(contacts[i]);
+						var substr = e.target.value;
+						if ((tmp.name.indexOf(substr) != -1) || (tmp.phone.indexOf(substr) != -1) || (tmp.email.indexOf(substr) != -1) || (tmp.address.indexOf(substr) != -1)){
+							resOfSearch += tmp.id + '%';
+						}
 					}
-				}
+				}	
 				
 				this.props.onChange(resOfSearch, true);
+				
 			}
-			else { this.props.onChange(resOfSearch, false);}
+			else this.props.onChange(resOfSearch, false);
 			this.setState({
 				userInput: e.target.value
 			});
